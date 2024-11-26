f1c06eb8589a9a2fa4a75cd28e9effc1a175e994
jBodzak <kubabodzak@o2.pl>
2024-11-26 16:32:57 +0100

    add aplication, add animation to button

--- app/src/main/java/com/quotes/ricknmortycards/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/quotes/ricknmortycards/ui/screen/HomeScreen.kt
@@ -1,5 +1,10 @@
 package com.quotes.ricknmortycards.ui.screen
 
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -16,10 +21,13 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -52,13 +60,28 @@ fun HomeScreen(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
             ) {
-                Box(
-                    modifier = Modifier
-                        .width(150.dp)
-                        .height(150.dp)
-                        .clip(CircleShape)
-                        .background(Color.Red)
-                ) {  }
+                PulsatingButton(
+                    animationContent = {
+                        Box(
+                            modifier = Modifier
+                                .width(150.dp)
+                                .height(150.dp)
+                                .clip(CircleShape)
+                                .background(Color.Red)
+                        )
+                    },
+                    content = {
+                        Box(
+                            modifier = Modifier
+                                .width(150.dp)
+                                .height(150.dp)
+                                .clip(CircleShape)
+                                .background(Color.Red)
+                        ) {
+
+                        }
+                    }
+                )
                 Box(
                     modifier = Modifier
                         .padding(top = 20.dp)
@@ -107,4 +130,40 @@ fun HomeScreen(
             }
         }
     }
+}
+
+@Composable
+fun PulsatingButton(
+    pulseFraction: Float = 1.2f,
+    alphaFraction: Float = 0.0f,
+    animationContent: @Composable () -> Unit,
+    content: @Composable () -> Unit) {
+
+    val infiniteTransition = rememberInfiniteTransition(label = "it")
+
+    val scale by infiniteTransition.animateFloat(
+        initialValue = 1f,
+        targetValue = pulseFraction,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1000),
+            repeatMode = RepeatMode.Restart
+        ), label = "its"
+    )
+
+    val alpha by infiniteTransition.animateFloat(
+        initialValue = 1f,
+        targetValue = alphaFraction,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1000),
+            repeatMode = RepeatMode.Restart
+        ), label = "ita"
+    )
+
+    Box {
+        Box(modifier = Modifier.scale(scale).alpha(alpha)) {
+            animationContent()
+        }
+        content()
+    }
+
 }
