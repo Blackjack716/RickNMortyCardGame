e65247decacb4c46eeaa020d2e78cff14fa744c3
jBodzak <kubabodzak@o2.pl>
2025-01-23 17:22:12 +0100

    simple card holder in collection, shimmer, navigation icons

--- app/src/main/java/com/rnm/ricknmortycards/ui/compose/NavigationBottomBar.kt
+++ app/src/main/java/com/rnm/ricknmortycards/ui/compose/NavigationBottomBar.kt
@@ -1,5 +1,6 @@
 package com.rnm.ricknmortycards.ui.compose
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -8,11 +9,17 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
+import com.rnm.ricknmortycards.R
 
 @Composable
 fun NavigationBottomBar(
@@ -35,7 +42,13 @@ fun NavigationBottomBar(
                 .weight(0.3f)
                 .clickable { onEvent(NavBarEvent.OnAllCardClicked) }
         ) {
-
+            Image(
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .size(30.dp),
+                imageVector = ImageVector.vectorResource(R.drawable.ic_navigation_collection),
+                contentDescription = null
+            )
         }
         Box(
             modifier = Modifier
@@ -45,7 +58,13 @@ fun NavigationBottomBar(
                 .weight(0.3f)
                 .clickable { onEvent(NavBarEvent.OnHomeClicked) }
         ) {
-
+            Image(
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .size(30.dp),
+                imageVector = ImageVector.vectorResource(R.drawable.ic_navigation_home),
+                contentDescription = null
+            )
         }
         Box(
             modifier = Modifier
@@ -55,7 +74,13 @@ fun NavigationBottomBar(
                 .weight(0.3f)
                 .clickable { onEvent(NavBarEvent.OnFavCardClicked) }
         ) {
-
+            Image(
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .size(30.dp),
+                imageVector = ImageVector.vectorResource(R.drawable.ic_navigation_favourites),
+                contentDescription = null
+            )
         }
     }
 }
--- app/src/main/java/com/rnm/ricknmortycards/ui/screen/AllCardsScreen.kt
+++ app/src/main/java/com/rnm/ricknmortycards/ui/screen/AllCardsScreen.kt
@@ -1,24 +1,48 @@
 package com.rnm.ricknmortycards.ui.screen
 
+import android.graphics.drawable.GradientDrawable
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TileMode
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
 import com.rnm.domain.model.Card
+import com.rnm.ricknmortycards.R
 import com.rnm.ricknmortycards.ui.compose.CurrencyCounterBar
 import com.rnm.ricknmortycards.utils.MockCardsData
 import com.rnm.ricknmortycards.ui.compose.NavBarEvent
 import com.rnm.ricknmortycards.ui.compose.NavigationBottomBar
+import com.rnm.ricknmortycards.ui.compose.shimmerLoadingAnimation
 
 @Preview(showBackground = true)
 @Composable
@@ -35,6 +59,18 @@ fun AllCardsScreen(
     onNavBardEvent: (NavBarEvent) -> Unit,
     allCardsState: List<Card> = emptyList(),
 ) {
+    val bottomTextColorStops = arrayOf(
+        0.0f to Color.Black,
+        0.5f to Color.Black,
+        1f to Color.Transparent
+    )
+
+    val bottomTextGradient = Brush.verticalGradient(
+        colorStops = bottomTextColorStops,
+        startY = Float.POSITIVE_INFINITY,
+        endY = 0.0f
+    )
+
     Box(
         modifier = modifier
             .fillMaxSize()
@@ -55,9 +91,92 @@ fun AllCardsScreen(
                 item {
                     Box(
                         modifier = Modifier
-                            .background(color = Color.Red)
+                            .height(180.dp)
+                            .background(color = Color.Cyan)
                     ) {
-                        Text("${it.name}")
+                        var isLoading by remember {
+                            mutableStateOf(true)
+                        }
+
+
+                        AsyncImage(
+                            model = it.photoUrl,
+                            contentDescription = it.name,
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .then(
+                                    if (isLoading) {
+                                        Modifier.shimmerLoadingAnimation(
+                                            isLoadingCompleted = false,
+                                            isLightModeActive = !isSystemInDarkTheme()
+                                        )
+                                    } else {
+                                        Modifier
+                                    }
+                                ),
+                            onLoading = {
+                                isLoading = true
+                            },
+                            onSuccess = {
+                                isLoading = false
+                            },
+                            onError = {
+                                isLoading = false
+                            },
+                            contentScale = ContentScale.FillWidth,
+                            alignment = Alignment.TopStart
+                        )
+                        Box(
+                            modifier = Modifier
+                                .align(Alignment.TopStart)
+                                .size(30.dp)
+                                .alpha(0.5f)
+                                .background(color = Color.LightGray, shape = RoundedCornerShape(4.dp))
+                        ) {
+                            Text("1")
+                        }
+                        Box(
+                            modifier = Modifier
+                                .align(Alignment.TopEnd)
+                                .size(30.dp)
+                                .alpha(0.5f)
+                                .background(color = Color.LightGray, shape = RoundedCornerShape(4.dp))
+                        ) {
+                            if (it.isFavourite == true) {
+                                Image(
+                                    modifier = Modifier
+                                        .align(Alignment.Center)
+                                        .size(24.dp),
+                                    imageVector = ImageVector.vectorResource(R.drawable.ic_star_favourite),
+                                    contentDescription = null
+                                )
+                            } else {
+                                Image(
+                                    modifier = Modifier
+                                        .align(Alignment.Center)
+                                        .size(24.dp),
+                                    imageVector = ImageVector.vectorResource(R.drawable.ic_star_no_favourite),
+                                    contentDescription = null
+                                )
+                            }
+
+                        }
+                        Box(
+                            modifier = Modifier
+                                .align(Alignment.BottomCenter)
+                                .fillMaxWidth()
+                                .alpha(0.4f)
+                                .background(brush = bottomTextGradient, shape = RoundedCornerShape(4.dp))
+                                .padding(top = 8.dp)
+                        ) {
+                            Text(
+                                text = "${it.name}",
+                                modifier = Modifier
+                                    .padding(horizontal = 4.dp, vertical = 2.dp)
+                                ,
+                                color = Color.White
+                            )
+                        }
                     }
                 }
             }
